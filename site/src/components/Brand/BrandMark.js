af866f5ea8ca4a387f2aae543be5f40d1cc39d20
gaurangrshah <gaurang.r.shah@gmail.com>
2020-03-05 21:15:24 -0500

    updates logo to single layer

--- site/src/components/Brand/BrandMark.js
+++ site/src/components/Brand/BrandMark.js
@@ -7,11 +7,9 @@ import Divider from "../../elements/divider"
 
 export const BrandMark = ({ offset }) => {
   return (
-    <Divider speed={0.2} offset={offset}>
+    <Divider speed={0.8} offset={offset}>
       <Box as='section' sx={{ maxWidth: '200' }}>
-        <Logo layer="drop" color='icon_green' left='10%' top='15%' width='250px' />
-        <Logo layer="g" color='icon_darkest' left='13.6%' top='18.6%' width='195px' />
-        <Logo layer="g" color='icon_blue' left='13.4%' top='18.5%' width='195px' />
+        <Logo layer='mark' left='10%' top='15%' width='250px' />
       </Box>
     </Divider>
   )
